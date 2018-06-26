4647d0cf28aa6da48fb95e165e00fab8f0382125
brianhkang1@gmail.com <brianhkang1@gmail.com>
2018-06-26 21:02:04 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,5 +3,4 @@ def display_rainbow(colors)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
 end
 
-bob     = ["Bob", 46, "Juggler", "555-555-5555"]
-stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
+rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
